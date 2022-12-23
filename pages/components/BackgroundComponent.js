11203f3a2cf8439b9cc6df72f02cd652c7e6b28f
treblig-punisher <gilbertplayx@hotmail.com>
2022-12-23 00:45:05 -0500

    Transition and Pokemon Name Display

--- pages/components/BackgroundComponent.js
+++ pages/components/BackgroundComponent.js
@@ -10,7 +10,16 @@ export default function BackgroundComponent() {
   const inputValComponent = useRef(null);
 
   const [currentPokemonPicture, setCurrentPokemonPicture] = useState('')
+  const [currentPokemonStats, setCurrentPokemonStats] = useState({})
 
+  const upperCasedWord = useCallback((value)=>{
+    let valueNow = value
+    if(valueNow !== undefined)
+    {
+      const upperCasedWord = valueNow.slice(0, 1).toUpperCase() + valueNow.slice(1)
+      return upperCasedWord
+    }
+  })
   const submittedPokemon = useCallback(async () => {
     const currentValue = inputValComponent.current.value;
     const lowerCasedValue = currentValue.toLowerCase();
@@ -18,6 +27,7 @@ export default function BackgroundComponent() {
     try {
       const getData = await fetch(`https://pokeapi.co/api/v2/pokemon/${lowerCasedValue}`)
       const convertToJson = await getData.json()
+      setCurrentPokemonStats(convertToJson)
       const getPokemonSprite = convertToJson.sprites.front_default
       setCurrentPokemonPicture(getPokemonSprite)
       console.log(getPokemonSprite)
@@ -25,20 +35,27 @@ export default function BackgroundComponent() {
     catch (err) {
       console.log(err + "pokemon not found")
       setCurrentPokemonPicture('/noMatch.png');
+      setCurrentPokemonStats({});
     }
   })
-  function PokemonDisplay({ imageToDisplay }) {
+  function PokemonDisplay({ imageToDisplay, type, pokeName }) {
     return (
       <div className={styles.pokemonDisplayContainer}>
-        <img src={imageToDisplay}></img>
+        <img draggable = {false} src={imageToDisplay}></img>
+        <div className={styles.pokemonType}>{type}</div>
+        <div className={styles.pokemonName}>{pokeName}</div>
       </div>
     );
   }
-  
+ 
   return (
     <div className={styles.bgImage}>
       {/* <PokemonDisplayContainer image={currentPokemonPicture} /> */}
-      <PokemonDisplay imageToDisplay={currentPokemonPicture} />
+      <PokemonDisplay
+        imageToDisplay={currentPokemonPicture}
+        type={currentPokemonStats.type}
+        pokeName={upperCasedWord(currentPokemonStats.name)}
+       />
       <DpadAndConfirmButtonsContainer buttonClickedFunction={submittedPokemon} />
       <SlimButtonsContainer />
       <div className={styles.greenScreen}>
--- pages/components/DpadComponent.js
+++ pages/components/DpadComponent.js
@@ -2,7 +2,7 @@ import styles from "/styles/DpadAndConfirmButtonsContainer.module.css"
 export default function DpadComponent() {
   return (
     <div className ={styles.Dpad}>
-        <img src="PokedexDpad.png"/>
+        <img draggable={false} src="PokedexDpad.png"/>
     </div>
   )
 }
